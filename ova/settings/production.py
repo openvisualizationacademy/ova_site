42975b10f94f77424e91924a44ec4fc76dd09a78
mastrong <32599005+mastrong@users.noreply.github.com>
2025-10-08 21:25:27 -0400

    Update production.py

--- ova/settings/production.py
+++ ova/settings/production.py
@@ -10,6 +10,9 @@ ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 try:
     from .local import *
 except ImportError:
