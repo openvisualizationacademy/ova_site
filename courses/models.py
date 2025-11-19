56f2dccccda8f8db35a5558bb97ec400786f8e90
Vinicius Sueiro <hi@vsueiro.com>
2025-11-19 13:20:57 -0300

    Add fixed list of tags (in order of importance), temporarily

--- courses/models.py
+++ courses/models.py
@@ -115,7 +115,9 @@ class CoursesIndexPage(Page):
         context['courses'] = courses
 
         # TODO: Consider sorting in order of importance
-        context['all_tags'] = sorted(tags)
+        # context['all_tags'] = sorted(tags)
+        # TEMP: Add fixed list of tags
+        context['all_tags'] = ["Essential", "Conceptual", "Technical"]
         return context
 
 
