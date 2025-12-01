d764a36ab695c65c5bcdd7ca58ccdbd1128d63ae
Vinicius Sueiro <hi@vsueiro.com>
2025-12-01 20:07:43 -0300

    Remember course overview toggle preference (whether details is open or not) and close on new pages when needed

--- ova/static/js/imports/Accessibility.js
+++ ova/static/js/imports/Accessibility.js
@@ -13,8 +13,27 @@ export default class Accessibility {
     this.reducedMotion = this.queries.reducedMotion.matches;
     this.queries.reducedMotion.addEventListener("change", (event) => (this.reducedMotion = event.matches));
 
+    // Remember course overview toggle preference (whether details is open or not)
+    this.setupOverviewState();
+
     // Simulate button behavior for `role=button`
-    document.addEventListener('keydown', this.simulateButton)
+    document.addEventListener('keydown', this.simulateButton);
+  }
+
+  setupOverviewState() {
+    const details = document.querySelector(".overview details");
+    if (!details) return;
+
+    // Moved snippet to HTML template for performance
+    // If users have last closed a course overview, keep new ones closed
+    // if (localStorage.getItem("overviewState") === "closed") {
+    //   details.open = false
+    // }
+
+    // If users open or close course overview, store that preference
+    details.addEventListener("toggle", () => {
+      localStorage.setItem("overviewState", details.open ? "open" : "closed");
+    });
   }
 
   // Based on https://stackoverflow.com/a/79664881
