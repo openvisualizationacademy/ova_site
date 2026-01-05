78cfcf854a360c0a1369c4438626e4535dbcbc5d
Melissa Strong <melissastrong89@gmail.com>
2026-01-05 12:16:55 -0500

    update logging for debugging in prod

--- ova/settings/production.py
+++ ova/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 import os
-
+from pathlib import Path
 
 DEBUG = False
 
@@ -17,16 +17,66 @@ try:
 except ImportError:
     pass
 
+
+LOG_DIR = Path(os.environ.get("DJANGO_LOG_DIR", Path(BASE_DIR) / "logs"))
+LOG_DIR.mkdir(parents=True, exist_ok=True)
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[{asctime}] {levelname} {name}: {message}",
+            "style": "{",
+        },
+    },
     "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": str(LOG_DIR / "app.log"),
+            "maxBytes": 10 * 1024 * 1024,
+            "backupCount": 5,
+            "formatter": "verbose",
+        },
+        "auth_email_file": {
+            "level": "DEBUG",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": str(LOG_DIR / "auth_email.log"),
+            "maxBytes": 10 * 1024 * 1024,
+            "backupCount": 5,
+            "formatter": "verbose",
+        },
         "console": {
+            "level": "ERROR",
             "class": "logging.StreamHandler",
+            "formatter": "verbose",
         },
     },
     "root": {
-        "handlers": ["console"],
-        "level": "DEBUG",
+        "handlers": ["file", "console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django.contrib.auth": {
+            "handlers": ["auth_email_file"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+        "django.core.mail": {
+            "handlers": ["auth_email_file"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+        "allauth": {
+            "handlers": ["auth_email_file"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+        "django.request": {
+            "handlers": ["file", "console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
     },
 }
