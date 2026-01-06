7d83b7e6820fb4660c226820af8c5ebd489fa9c2
Melissa Strong <melissastrong89@gmail.com>
2026-01-06 18:36:36 -0500

    force key in dev env

--- ova/settings/dev.py
+++ ova/settings/dev.py
@@ -10,8 +10,7 @@ if not DEBUG:
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-@$7dr8z96_o%u067c#(nt$k7b60v+_%306qb2azc+145!0n19^",
+    "DJANGO_SECRET_KEY",""
 )
 
 # SECURITY WARNING: define the correct hosts in production!
