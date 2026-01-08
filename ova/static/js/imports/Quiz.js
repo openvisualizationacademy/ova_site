3b5e245d0187b9fbc14a8520b941d44e2470a349
Vinicius Sueiro <hi@vsueiro.com>
2026-01-08 17:25:15 -0300

    Improve quiz markup (no submission after all correct) and prevent flash of unstyled choices on submit (due to disabled attribute being removed)

--- ova/static/js/imports/Quiz.js
+++ ova/static/js/imports/Quiz.js
@@ -2,13 +2,27 @@ export default class Quiz {
   constructor(course, selector) {
     this.course = course;
 
-    this.element = this.course.element.querySelector(selector);
-    if (!this.element) return;
+    this.form = this.course.element.querySelector(selector);
+    if (!this.form || this.form.tagName != "FORM") return;
 
     this.setup();
   }
 
+  enableFields() {
+
+    // Enable all form elements so they get sent to backend
+    this.form.querySelectorAll(":disabled").forEach((element) => {
+      element.disabled = false;
+    });
+  }
+ 
   setup() {
-    console.log("Quiz class is setup");
+    
+    // When quiz form gets submitted
+    this.form.addEventListener("submit", () => {
+      this.enableFields();
+
+      // TODO: Add spinner and announce “Submitting” to screen readers
+    });
   }
 }
