074afd2ddf1267a4756cf0e6841f6812e18e6aba
Vinicius Sueiro <hi@vsueiro.com>
2026-01-09 19:28:20 -0300

    Combine `instructors` and `contributors` JS classes and Django includes into a single `people` class and include

--- ova/static/js/imports/Contributors.js
+++ ova/static/js/imports/Contributors.js
@@ -4,30 +4,4 @@ export default class Contributors extends People {
   constructor(app, selector) {
     super(app, selector);
   }
-
-  getDetails(person) {
-    const alt = person.alt ? person.alt : "Profile";
-
-    return `
-    <div class="details">
-      <div class="person" data-role="${this.role}">
-        ${
-          person.photo
-            ? `<img class="media" src="/static/media/people/small/${person.photo}" loading="lazy" alt="${alt}">`
-            : `<div class="media"></div>`
-        }
-        <div class="info">
-          <h2><strong>${person.name}</strong></h2>
-          <span class="tagline">${person.tagline}</span>
-          <p class="bio">${person.bio}</p>
-
-          ${ this.getCourses(person) }
-        </div>
-        <ul class="links small">
-          ${ this.list(person.links) }
-        </ul>
-      </div>
-    </div>
-    `;
-  }
 }
--- ova/static/js/imports/People.js
+++ ova/static/js/imports/People.js
@@ -1,5 +1,3 @@
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
-
 export default class People {
   constructor(app, selector) {
     this.app = app;
